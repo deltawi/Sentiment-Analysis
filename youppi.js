d7e2bd74e4fc36d332e1f076ad9a87ca15a90cf0
deltawi <tantaoui.mehdi@gmail.com>
2016-02-07 16:55:23 +0000

    Cleaning code

--- youppi.js
+++ youppi.js
@@ -60,12 +60,9 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
   Meteor.startup(function () {
-<<<<<<< HEAD
   /*HTTP.call( 'POST', 'http://jsonplaceholder.typicode.com/posts', { headers: { 'Content-Type' : 'application/json;charset=UTF-8'  } }, function( error, response ) {
     console.log(response)
   });*/
-=======
->>>>>>> cacbfe8af9fe0c32511b7b6fc5d5acf81c159c75
 
   console.log("Server started")
 });
@@ -75,33 +72,10 @@ if (Meteor.isServer) {
 
   'calc': function (a, b) {
 
-<<<<<<< HEAD
   var url = 'https://palbyp.pmservice.ibmcloud.com/pm/v1/score/addition?accesskey=xTPKrR8J/rEBEHzVoGfKZsI1ZxFoP6idoPkIojJMxupqvncuZFzmoBCP7iTHwY5HHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+GkQgZ6p+lt2AGo/g8p66bV3nWkl/5WtqQaT01IrXihgzKJ6iSsYCKch1YtvJeVcE0='
-=======
-  var url = 'https://palbyp.pmservice.ibmcloud.com/pm/v1/score/addition?accesskey=xTPKrR8J/rEBEHzVoGfKZsI1ZxFoP6idoPkIojJMxupqvncuZFzmoBCP7iTHwY5HHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+EeiOOg65iU48SsfY8Mnv2a9wLWhajGNS2stuWToEpRSjCk2/tVsJWBfDpmkYJAdbM=';
 
-  var req = {"tablename":"additionInput.txt","header":'["A","B"]',"data":'[["3", "2"]]'};
+  var req = {"tablename":"additionInput.txt","header":'["A","B"]',"data":'[["3", "2"]]'}
 
-
-
-  var input = {
-    tablename : 'additionInput.txt',
-    header : ['A', 'B'],
-    data : [['1', '2']]
-  };
-
-
-  var options = {
-    headers: {'Content-Type': 'application/json'},
-    params: JSON.stringify(input)
-  };
->>>>>>> cacbfe8af9fe0c32511b7b6fc5d5acf81c159c75
-
-  
-  
-        
-
-<<<<<<< HEAD
   var input = {
         tablename: 'additionInput.txt',
         header: [ 'A',  'B'],
@@ -111,20 +85,12 @@ if (Meteor.isServer) {
   var header= { 'Content-Type' : 'application/json;charset=UTF-8'  }
   HTTP.call( 'POST', url, { headers: { 'Content-Type' : 'application/json;charset=UTF-8'  },data:input }, function( error, response ) {
         console.log("====================>")
-=======
-  HTTP.call( 'POST', url, { options }, function( error, response ) {
-        console.log("YES WERE ARE SWAG")
->>>>>>> cacbfe8af9fe0c32511b7b6fc5d5acf81c159c75
         console.log(response)
         //console.log(error)
 
   });
 
 
-
-  }
-
+}
 });
-
-
 }
